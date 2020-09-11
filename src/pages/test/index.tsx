bd7fc49e6ee9e5d1def7600db416e452db4c4e9b
ningzhenyu <zhenyu_ning@qingsongchou.com>
2020-09-11 14:48:13 +0800

    watchEffect 这个好怪

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,4 +1,5 @@
-import { defineComponent } from 'vue';
+import { defineComponent, reactive, watchEffect, isRef } from 'vue';
+import { Button } from 'ant-design-vue';
 import SlotChild from './slot';
 import SlotChildSfc from './slot.vue';
 
@@ -6,17 +7,31 @@ import SlotChildSfc from './slot.vue';
 export default defineComponent({
   name: 'Test',
   setup() {
+    const state = reactive({ count: 0 });
+
+    const add = () => {
+      state.count += 1;
+    }
+
+    console.log('isRef:', isRef(state));
+
+    watchEffect(() => {
+      console.log('watchEffect:', state.count)
+    })
+
     return () => (
       <div>
         只有在子组件中声明了 props ， 才会作为 props 传入
-        <hr/>
-        <SlotChildSfc  name='sfc slot' number={1} onEvent={() => { console.log('slot sfc parent click') }}>
+        <hr />
+        <SlotChildSfc name='sfc slot' number={1} onEvent={() => { console.log('slot sfc parent click') }}>
           {{ default: () => (<span>default slot</span>), name: () => (<span>name slot</span>) }}
         </SlotChildSfc>
-        <hr/>
+        <hr />
         <SlotChild name='slot' number={1} onEvent={() => { console.log('slot parent click') }}>
           {{ default: () => (<span>default slot</span>), name: () => (<span>name slot</span>) }}
         </SlotChild>
+        <hr />
+        <Button type='danger' onClick={add}>喜加一</Button>
       </div>
     )
   }
