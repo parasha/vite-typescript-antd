fa95716970a8b7043d8f6b012f71569da7231be2
宁振宇 <ningzhenyu@youzan.com>
2021-02-26 19:15:55 +0800

    这块没写完

--- src/pages/account/register.vue
+++ src/pages/account/register.vue
@@ -1,50 +1,64 @@
 <template>
   <p class="account-window-title">账号注册</p>
-  <a-input
-    type="text"
-    v-model="formData.username"
-    class="input-line"
-    placeholder="请输入帐号"
-    :allowClear="true"
-  >
-    <template #prefix>
-      <UserOutlined class="label-icon" />
-    </template>
-  </a-input>
-	<a-input
-    type="text"
-    v-model="formData.nickname"
-    class="input-line"
-    placeholder="请输入用户名"
-    :allowClear="true"
-  >
-		<template #prefix>
-      <SmileOutlined class="label-icon" />
-    </template>
-	</a-input>
-  <a-input
-    type="password"
-    v-model="formData.password"
-    class="input-line"
-    placeholder="请输入密码"
-    :allowClear="true"
-  >
-    <template #prefix>
-      <KeyOutlined class="label-icon" />
-    </template>
-  </a-input>
-  <a-input
-    type="password"
-    v-model="formData.checkPassword"
-    class="input-line"
-    placeholder="请再次输入密码"
-    :allowClear="true"
-  >
-    <template #prefix>
-      <KeyOutlined class="label-icon" />
-    </template>
-  </a-input>
-  <a-button class="submit-button" type="primary" size="large">注册</a-button>
+  <a-form :model="formData">
+    <a-form-item>
+      <a-input
+        type="text"
+        v-model:value="formData.username"
+        class="input-line"
+        placeholder="请输入帐号"
+        :allowClear="true"
+      >
+        <template #prefix>
+          <UserOutlined class="label-icon" />
+        </template>
+      </a-input>
+    </a-form-item>
+    <a-form-item>
+      <a-input
+        type="text"
+        v-model:value="formData.nickname"
+        class="input-line"
+        placeholder="请输入用户名"
+        :allowClear="true"
+      >
+        <template #prefix>
+          <SmileOutlined class="label-icon" />
+        </template>
+      </a-input>
+    </a-form-item>
+    <a-form-item>
+      <a-input
+        type="password"
+        v-model:value="formData.password"
+        class="input-line"
+        placeholder="请输入密码"
+        :allowClear="true"
+      >
+        <template #prefix>
+          <KeyOutlined class="label-icon" />
+        </template>
+      </a-input>
+    </a-form-item>
+    <a-form-item>
+      <a-input
+        type="password"
+        v-model="formData.checkPassword"
+        class="input-line"
+        placeholder="请再次输入密码"
+        :allowClear="true"
+      >
+        <template #prefix>
+          <KeyOutlined class="label-icon" />
+        </template>
+      </a-input>
+    </a-form-item>
+    <a-form-item>
+      <a-button class="submit-button" type="primary" size="large" @click='register'
+        >注册</a-button
+      >
+    </a-form-item>
+  </a-form>
   <div class="others flex">
     <RouterLink to="/account/login" replace>返回登录</RouterLink>
   </div>
@@ -53,7 +67,7 @@
 <script lang='ts'>
 import { defineComponent, reactive } from "vue";
 import { useStore } from "vuex";
-import { Input, Button } from "ant-design-vue";
+import { Input, Button, Form } from "ant-design-vue";
 import {
   UserOutlined,
   KeyOutlined,
@@ -65,6 +79,8 @@ export default defineComponent({
   components: {
     [Input.name]: Input,
     [Button.name]: Button,
+    [Form.name]: Form,
+    [Form.Item.name]: Form.Item,
     UserOutlined,
     KeyOutlined,
     SmileOutlined,
@@ -77,11 +93,40 @@ export default defineComponent({
       nickname: "",
     });
 
-    return { formData };
+    function checkBeforeFetchRegister (){
+      console.log('校验：', formData);
+      return true;
+    }
+
+    function register () {
+      if(checkBeforeFetchRegister()){
+
+      }else{
+        
+      }
+    }
+
+    return { formData, register };
   },
 });
 </script>
 
-<style lang='less'>
+<style lang='less' scoped>
 @import "./index.module.less";
+
+.input-line {
+  width: 320px;
+  margin-bottom: 0;
+}
+
+.submit-button{
+  width: 162px;
+}
+
+.ant-form-item{
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 </style>
